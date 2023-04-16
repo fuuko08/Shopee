858a7b7b943daaebc220428f4c56b5418ebd89ae
funfun <phongsu20@gmail.com>
2023-04-15 19:41:08 -0700

    delete function

--- Shopee/Controllers/CategoryController.cs
+++ Shopee/Controllers/CategoryController.cs
@@ -7,7 +7,6 @@ namespace Shopee.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
-
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
@@ -58,19 +57,40 @@ namespace Shopee.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name!");
-            }
-
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View();
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }            
+             _db.Categories.Remove(obj);
+             _db.SaveChanges();
+             return RedirectToAction("Index");
+        }
     }
 }
-
